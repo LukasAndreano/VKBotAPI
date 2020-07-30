4e8ab1ad4017cc76d25100610ecf5e5c88a4ee5c
Nikita <52691731+LukasAndreano@users.noreply.github.com>
2020-07-30 23:35:37 +0600

    Исправление бага и добавление новой функции
    - Исправлен баг с SendOK();
    - Добавлена функция SendUsersButton

--- vk.php
+++ vk.php
@@ -66,6 +66,50 @@ class VK {
 		$this->Request("messages.send", array("peer_id" => $peer_id, "message" => $message, "attachment" => $attachment, "random_id" => $this->random_id, "keyboard" => $buttons));
 	}
 
+    public function SendUsersButton($user_ids, $message, $gl_massiv=array(), $inline, $attachment = null) {
+        $buttons = [];
+        $i = 0;
+        foreach ($gl_massiv as $button_str) {
+            $j = 0;
+            foreach ($button_str as $button) {
+                if ($button[0] == 'text') {
+                    $color = $this->replaceColor($button[3]);
+                    $buttons[$i][$j]["action"]["type"] = "text";
+                    if ($button[1] != null)
+                        $buttons[$i][$j]["action"]["payload"] = json_encode($button[1], JSON_UNESCAPED_UNICODE);
+                    $buttons[$i][$j]["action"]["label"] = $button[2];
+                    $buttons[$i][$j]["color"] = $color;
+                } if ($button[0] == 'link') {
+                    $buttons[$i][$j]["action"]["type"] = "open_link";
+                    $buttons[$i][$j]["action"]["label"] = $button[1];
+                    $buttons[$i][$j]["action"]["link"] = $button[2];
+                } if ($button[0] == 'location') {
+                    $buttons[$i][$j]["action"]["type"] = "location";
+                } if ($button[0] == 'callback') {
+                    $buttons[$i][$j]["action"]["type"] = "callback";
+                    if ($button[1] != null)
+                        $buttons[$i][$j]["action"]["payload"] = json_encode($button[1], JSON_UNESCAPED_UNICODE);
+                    $buttons[$i][$j]["action"]["label"] = $button[2];
+                    $color = $this->replaceColor($button[3]);
+                    $buttons[$i][$j]["color"] = $color;
+                }
+                $j++;
+            }
+            $i++;
+        }
+        if ($inline == true) {
+            $one_time = false;
+        } else {
+            $one_time = true;
+        }
+        $buttons = array(
+            "one_time" => $one_time,
+            "inline" => $inline,
+            "buttons" => $buttons);
+        $buttons = json_encode($buttons, JSON_UNESCAPED_UNICODE);
+        $this->Request("messages.send", array("user_ids" => $user_ids, "message" => $message, "attachment" => $attachment, "random_id" => $this->random_id, "keyboard" => $buttons));
+    }
+
 	public function SendEvent($user_id, $peer_id, $event_id, $payload) {
 		$this->Request("messages.sendMessageEventAnswer", array("user_id" => $user_id, "peer_id" => $peer_id, "event_id" => $event_id, "event_data" => json_encode($payload)));
 	}
@@ -92,5 +136,33 @@ class VK {
 		$request = json_decode(file_get_contents($this->endpoint."/$method?".http_build_query($params)."&access_token=".$this->token."&v=".$this->version), true);
 		return $request;
 	}
+
+    public function sendOK(){
+        echo 'ok';
+        $response_length = ob_get_length();
+        // check if fastcgi_finish_request is callable
+        if (is_callable('fastcgi_finish_request')) {
+            /*
+             * This works in Nginx but the next approach not
+             */
+            session_write_close();
+            fastcgi_finish_request();
+
+            return;
+        }
+
+        ignore_user_abort(true);
+
+        ob_start();
+        $serverProtocole = filter_input(INPUT_SERVER, 'SERVER_PROTOCOL', FILTER_SANITIZE_STRING);
+        header($serverProtocole.' 200 OK');
+        header('Content-Encoding: none');
+        header('Content-Length: '. $response_length);
+        header('Connection: close');
+
+        ob_end_flush();
+        ob_flush();
+        flush();
+    }
 
 }
